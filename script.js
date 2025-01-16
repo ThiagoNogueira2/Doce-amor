c7b89cdc55aaf10bc642a71e3fd52dfa0cb744fa
Thiaguinnnnnnnn <136134601+Thiaguinnnnnnnn@users.noreply.github.com>
2025-01-15 23:51:06 -0300

    docinhos-de-festa-responsivo

---
+++ script.js
@@ -0,0 +1,5 @@
+// Exemplo de JavaScript se necessário para funcionalidades adicionais
+document.addEventListener('DOMContentLoaded', function () {
+    // Coloque qualquer código adicional que você precise aqui
+  });
+  
